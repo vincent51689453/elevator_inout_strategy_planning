e42da41053d97238eb9eb215c1eb3bb963b97e02
vincen51689453 <vincent51689453@gmail.com>
2021-10-22 14:36:31 +0800

    update algo and plan to perform control

--- src/vision_control/src/pcl_control.cpp
+++ src/vision_control/src/pcl_control.cpp
@@ -31,11 +31,13 @@ ros::Subscriber pcl_sub;                                        // Navigation Ve
 // User parameters
 double _y = 0;                                                  // Previous y of a point
 int frame_id = 0;                                               // Frame index
-double max_d = 0;                                               // Maximum gap
-double mid_y = 0;                                               // mid_y of the gap
-double mid_x = 0;                                               // mid_x of the gap (depth)
-const double robot_Width = 0.55;                                // Robot Width
-
+double max_d = 0;                                               // Maximum gap in the cloud
+double gap_mid = 0;                                             // Mid point of the max gap
+double gap_depth = 0;                                           // Depth of the max gap
+const double right_max = -0.8;                                  // Margin for robot vision of right
+const double left_max = 0.8;                                    // Margin for robot vision of left
+const double z_cutoff = 1;                                      // Z cutoff distance in meter
+enum gapType {PCL_ALGO, LEFT_MARGIN, RIGHT_MARGIN,EMPTY}        // Gap type
 /*
  * Coordinate system in PCL
  * Green : Y (PCL Left/Right)
@@ -44,20 +46,54 @@ const double robot_Width = 0.55;                                // Robot Width
  * 
 */
 
-
-
+void robot_control(distance,direction,gapType gap)
+{
+    // ROBOT CONTROL
+}
 
 void cloud_callback (const sensor_msgs::PointCloud2 &cloud_msg)
 {
     std::map<double,double> linear_scan;
     std::map<double,double>::iterator iter;
 
+    // Left Margin Marker
+    visualization_msgs::Marker left_limit_marker;
+    left_limit_marker.header.frame_id = "/camera_link";
+    left_limit_marker.header.stamp = ros::Time::now();
+    left_limit_marker.ns = "basic_shapes";
+    left_limit_marker.id = 0;
+    left_limit_marker.type = visualization_msgs::Marker::ARROW;
+    left_limit_marker.action = visualization_msgs::Marker::ADD;
+    left_limit_marker.lifetime = ros::Duration(); 
+
+    // Left Margin Marker end point
+    geometry_msgs::Point left_limit_pt;
+    left_limit_pt.x = 1.0;
+    left_limit_pt.y = left_max;
+    left_limit_pt.z = 0;
+
+    // Right Margin Marker
+    visualization_msgs::Marker right_limit_marker;
+    right_limit_marker.header.frame_id = "/camera_link";
+    right_limit_marker.header.stamp = ros::Time::now();
+    right_limit_marker.ns = "basic_shapes";
+    right_limit_marker.id = 1;
+    right_limit_marker.type = visualization_msgs::Marker::ARROW;
+    right_limit_marker.action = visualization_msgs::Marker::ADD;
+    right_limit_marker.lifetime = ros::Duration();       
+
+    // Right Margin Marker end point
+    geometry_msgs::Point right_limit_pt;
+    right_limit_pt.x = 1.0;
+    right_limit_pt.y = right_max;
+    right_limit_pt.z = 0;
+
     // Navigation Marker
     visualization_msgs::Marker navigation_marker;
     navigation_marker.header.frame_id = "/camera_link";
     navigation_marker.header.stamp = ros::Time::now();
     navigation_marker.ns = "basic_shapes";
-    navigation_marker.id = 0;
+    navigation_marker.id = 2;
     navigation_marker.type = visualization_msgs::Marker::ARROW;
     navigation_marker.action = visualization_msgs::Marker::ADD;
     navigation_marker.lifetime = ros::Duration();
@@ -78,8 +114,10 @@ void cloud_callback (const sensor_msgs::PointCloud2 &cloud_msg)
     sensor_msgs::convertPointCloud2ToPointCloud(cloud_msg,obstacle_cloud);
 
     // Store all points data(y,x) into a vector for path planning, eliminate y axis
+    bool valid_linear_scan = false;
     for(int i=0;i<obstacle_cloud.points.size();i++)
     {
+
         //Show XYZ data
         //std:: cout << "[DEBUG Point #" << i << std::endl;
         //std:: cout << "x:" << obstacle_cloud.points[i].x << " y:" << obstacle_cloud.points[i].y << " z:" << obstacle_cloud.points[i].z << std::endl; 
@@ -92,46 +130,138 @@ void cloud_callback (const sensor_msgs::PointCloud2 &cloud_msg)
         double pointcloudY = -1*x;
         double pointcloudZ = -1*y;
 
-        // Only consider Y axis [left & right] and X axis [depth]
-        linear_scan.insert(std::make_pair(pointcloudY,pointcloudZ));
-    }
-
-    // Searching for 'GAP' between obstacles
-    for(iter=linear_scan.begin();iter!=linear_scan.end();iter++)
-    {
-        double d = 0.0;
-        // Find distance between previous point and current point along Y
-        (iter->first>_y)?d = iter->first-_y:d=0;
-
-
-        // Find maximum d
-        if(d>max_d)
+        if(pointcloudX < z_cutoff)
         {
-            max_d = d;
-            mid_y = (iter->first+_y)*0.5;
-            // Update navigation_marker end points
-            arrow_end.x = 0.6;
-            arrow_end.y = mid_y;
-            arrow_end.z = 0;        
+            // Only consider Y axis [left & right] and X axis [depth]
+            linear_scan.insert(std::make_pair(pointcloudY,pointcloudZ));
+            valid_linear_scan = true;
         }
-
-        // Update previous value
-        _y = iter->first;
-
     }
+
+    // If no valid linear_scan, just go forward
+    if(valid_linear_scan)
+    {
+        // Searching for 'GAP' between obstacles
+        for(iter=linear_scan.begin();iter!=linear_scan.end();iter++)
+        {
+            double d = 0.0;
+            // Find distance between previous point and current point along Y
+            (iter->first>_y)?d = iter->first-_y:d=0;
+
+
+            // Find maximum d
+            if(d>max_d)
+            {
+                max_d = d;
+                gap_mid = (iter->first+_y)*0.5; 
+                gap_depth = (iter->second);     
+            }
 
             
-    //Publish navigation marker
-    navigation_marker.points.push_back(arrow_start);
-    navigation_marker.points.push_back(arrow_end);
-    navigation_marker.scale.x = 0.03;
-    navigation_marker.scale.y = 0.05;
-    navigation_marker.scale.z = 0.1;
-    navigation_marker.color.r = 0.2;
-    navigation_marker.color.g = 1.0;
-    navigation_marker.color.b = 1.0;
-    navigation_marker.color.a = 1.0;
-    navigate_marker_pub.publish(navigation_marker);
+            // Update previous value
+            _y = iter->first;
+
+        }
+
+        // Calculating marginal spaces
+        double left_margin_d = 0.0;
+        double right_margin_d = 0.0;
+        double start_y = 0.0;
+        double start_z = 0.0;
+        double end_y = 0.0;
+        double end_z = 0.0;
+
+        iter = linear_scan.begin();
+        left_margin_d = abs(iter->first - left_max);
+        start_y = iter->first;
+        start_z = iter->second;
+
+        iter = linear_scan.end();
+        iter --;
+        right_margin_d = abs(right_max - iter->first);
+        end_y = iter->first;
+        end_z = iter->second;
+
+        // Show resutls
+        std::cout << "Left Margin Distance: " << left_margin_d << " | Right Margin Distance: " << right_margin_d << std::endl;
+        std::cout << "PCL Gap Distance: " << max_d << std::endl;
+
+        // Determine max_d or marginal spaces should be used
+        if(((right_margin_d)>left_margin_d)&&((right_margin_d)>max_d))
+        {
+            gap_mid = (right_max+end_y)*0.5;
+            gap_depth = end_z;
+            // Show determined solution
+            std::cout << "[OUTPUT] Right margin space -> mid: " << gap_mid << " | depth: " << gap_depth << std::endl;
+        }
+        else if(((left_margin_d)>right_margin_d)&&((left_margin_d)>max_d))
+        {
+            gap_mid = (left_max+start_y)*0.5;
+            gap_depth = start_z;
+            // Show determined solution
+            std::cout << "[OUTPUT] Left margin space -> mid: " << gap_mid << " | depth: " << gap_depth << std::endl;
+        }else
+        {
+            std::cout << "[OUTPUT] PCL space -> mid: " << gap_mid << " | depth: " << gap_depth << std::endl;
+        }
+        std::cout << std::endl;
+        // Publish navigation marker
+        arrow_end.x = 1.0;
+        arrow_end.y = gap_mid;
+        arrow_end.z = 0;  
+        navigation_marker.points.push_back(arrow_start);
+        navigation_marker.points.push_back(arrow_end);
+        navigation_marker.scale.x = 0.03;
+        navigation_marker.scale.y = 0.05;
+        navigation_marker.scale.z = 0.1;
+        navigation_marker.color.r = 0.2;
+        navigation_marker.color.g = 1.0;
+        navigation_marker.color.b = 1.0;
+        navigation_marker.color.a = 1.0;
+        navigate_marker_pub.publish(navigation_marker);
+
+    }else{
+        std::cout << "[OUTPUT] Out of range: FORWARD!" << std::endl;
+        std::cout << std::endl;
+        // Publish navigation marker
+        arrow_end.x = 1.0;
+        arrow_end.y = 0;
+        arrow_end.z = 0;  
+        navigation_marker.points.push_back(arrow_start);
+        navigation_marker.points.push_back(arrow_end);
+        navigation_marker.scale.x = 0.03;
+        navigation_marker.scale.y = 0.05;
+        navigation_marker.scale.z = 0.1;
+        navigation_marker.color.r = 0.2;
+        navigation_marker.color.g = 1.0;
+        navigation_marker.color.b = 1.0;
+        navigation_marker.color.a = 1.0;
+        navigate_marker_pub.publish(navigation_marker);
+    }
+
+    // Publish right margin marker 
+    right_limit_marker.points.push_back(arrow_start);
+    right_limit_marker.points.push_back(right_limit_pt);
+    right_limit_marker.scale.x = 0.03;
+    right_limit_marker.scale.y = 0.05;
+    right_limit_marker.scale.z = 0.1;
+    right_limit_marker.color.r = 1.0;
+    right_limit_marker.color.g = 0.4;
+    right_limit_marker.color.b = 1.0;
+    right_limit_marker.color.a = 1.0;
+    navigate_marker_pub.publish(right_limit_marker);   
+
+    // Publish left margin marker 
+    left_limit_marker.points.push_back(arrow_start);
+    left_limit_marker.points.push_back(left_limit_pt);
+    left_limit_marker.scale.x = 0.03;
+    left_limit_marker.scale.y = 0.05;
+    left_limit_marker.scale.z = 0.1;
+    left_limit_marker.color.r = 1.0;
+    left_limit_marker.color.g = 0.4;
+    left_limit_marker.color.b = 1.0;
+    left_limit_marker.color.a = 1.0;
+    navigate_marker_pub.publish(left_limit_marker);  
      
     frame_id++;
 }
